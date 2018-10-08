09bc17580c7e5d63b58ad1108c809fef53561f38
daiwenlong <dwl0208@163.com>
2018-10-08 20:37:42 +0800

    Initial commit

---
+++ src/main/java/com/dwl/mindoc/MindocApplication.java
@@ -0,0 +1,12 @@
+package com.dwl.mindoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindocApplication.class, args);
+	}
+}
---
+++ src/test/java/com/dwl/mindoc/MindocApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dwl.mindoc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MindocApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
